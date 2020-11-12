8995381bc3319de3688bb52ee29d339326a69503
Robert Clarke <r.jason.clarke@gmail.com>
2020-11-12 16:14:23 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :name :artist :genre
+
+end
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+
+ninety_nine_problems.name
+  # => "99 Problems"
+
+ninety_nine_problems.artist
+  # => "Jay-Z"
+
+ninety_nine_problems.genre
+  # => "rap"
