9e4da029dc4f6b3127af91124a9669f11b704822
Robert Clarke <r.jason.clarke@gmail.com>
2020-11-12 20:02:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,27 +1,55 @@
+
 class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@count = 0
+  @@song_count = 0
   @@genres = []
   @@artists = []
 
   def initialize(name, artist, genre)
-  @name = name
-  @artist = artist
-  @genre = genre
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count +=1
   end
 
   def self.count
-    @@count
+    @@song_count
   end
 
   def self.genres
-    @@genre.uniq
+    @@genres.uniq
   end
 
   def self.artists
-    @@artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+      genre_count[genre] += 1
+      else
+      genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+      artist_count[artist] +=1
+      else
+      artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
 end
